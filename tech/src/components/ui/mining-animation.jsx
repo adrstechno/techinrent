733856d0e73db8e8f5d4ca94ad168e1ab0b965ef
Sachin Sen <sachinsen1920@gmail.com>
2025-08-02 03:14:18 +0530

    Update mining animation component

--- tech/src/components/ui/mining-animation.jsx
+++ tech/src/components/ui/mining-animation.jsx
@@ -1,21 +1,21 @@
-const React = require("react")
-const { cn } = require("@/lib/utils")
+import { createElement } from "react"
+import { cn } from "@/lib/utils"
 
 const MiningAnimation = ({ hashRate, className, ...props }) => {
-  return React.createElement("div", {
+  return createElement("div", {
     className: cn("flex items-center space-x-2", className),
     ...props
-  }, React.createElement("div", {
+  }, createElement("div", {
     className: "flex items-center space-x-1"
-  }, React.createElement("div", {
+  }, createElement("div", {
     className: "w-2 h-2 bg-green-500 rounded-full animate-pulse"
-  }), React.createElement("span", {
+  }), createElement("span", {
     className: "text-sm text-muted-foreground"
-  }, "Mining")), React.createElement("p", {
+  }, "Mining")), createElement("p", {
     className: "text-sm font-medium"
   }, typeof hashRate === 'number' ? hashRate.toFixed(0) : '0', " TH/s"))
 }
 
 MiningAnimation.displayName = "MiningAnimation"
 
-module.exports = { MiningAnimation }
+export  { MiningAnimation }
