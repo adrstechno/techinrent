e1311abd6310051b7142731e13f7afb7501887d1
Sachin Sen <sachinsen1920@gmail.com>
2025-08-01 17:35:13 +0530

    fix import button

--- tech/src/components/ui/input.jsx
+++ tech/src/components/ui/input.jsx
@@ -1,8 +1,8 @@
-const React = require("react")
-const { cn } = require("@/lib/utils")
+import { forwardRef, createElement } from "react"
+import { cn } from "@/lib/utils"
 
-const Input = React.forwardRef(({ className, type, ...props }, ref) => {
-  return React.createElement("input", {
+const Input = forwardRef(({ className, type, ...props }, ref) => {
+  return createElement("input", {
     type: type,
     className: cn(
       "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
@@ -14,5 +14,5 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
 })
 Input.displayName = "Input"
 
-module.exports = { Input }
+export default { Input }
 
