e1de44851bcf96f5d8a333dc82ec0e88dc93b464
Sachin Sen <sachinsen1920@gmail.com>
2025-08-01 17:40:39 +0530

    Add collapsible UI component

---
+++ tech/src/components/ui/collapsible.jsx
@@ -0,0 +1,11 @@
+const CollapsiblePrimitive = require("@radix-ui/react-collapsible")
+
+const Collapsible = CollapsiblePrimitive.Root
+const CollapsibleTrigger = CollapsiblePrimitive.Trigger
+const CollapsibleContent = CollapsiblePrimitive.Content
+
+module.exports = {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+}
