be79b217cd251b3579d5cc2e3de763e2f14dca39
Sachin Sen <sachinsen1920@gmail.com>
2025-08-02 03:16:31 +0530

    Update switch component

--- tech/src/components/ui/switch.jsx
+++ tech/src/components/ui/switch.jsx
@@ -1,21 +1,21 @@
-const React = require("react")
-const SwitchPrimitives = require("@radix-ui/react-switch")
-const { cn } = require("@/lib/utils")
+import { forwardRef, createElement } from "react"
+import { Root, Thumb } from "@radix-ui/react-switch"
+import { cn } from "@/lib/utils"
 
-const Switch = React.forwardRef(({ className, ...props }, ref) => {
-  return React.createElement(SwitchPrimitives.Root, {
+const Switch = forwardRef(({ className, ...props }, ref) => {
+  return createElement(Root, {
     className: cn(
       "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
       className
     ),
     ...props,
     ref: ref
-  }, React.createElement(SwitchPrimitives.Thumb, {
+  }, createElement(Thumb, {
     className: cn(
       "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
     )
   }))
 })
-Switch.displayName = SwitchPrimitives.Root.displayName
+Switch.displayName = Root.displayName
 
-module.exports = { Switch }
+export  { Switch }
