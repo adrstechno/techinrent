feee52bba4dec844ec2331a624f5f78f88e0f9dc
Sachin Sen <sachinsen1920@gmail.com>
2025-08-02 03:16:21 +0530

    Update slider component

--- tech/src/components/ui/slider.jsx
+++ tech/src/components/ui/slider.jsx
@@ -1,23 +1,23 @@
-const React = require("react")
-const SliderPrimitive = require("@radix-ui/react-slider")
-const { cn } = require("@/lib/utils")
+import { forwardRef, createElement } from "react"
+import { Root, Track, Range, Thumb } from "@radix-ui/react-slider"
+import { cn } from "@/lib/utils"
 
-const Slider = React.forwardRef(({ className, ...props }, ref) => {
-  return React.createElement(SliderPrimitive.Root, {
+const Slider = forwardRef(({ className, ...props }, ref) => {
+  return createElement(Root, {
     ref: ref,
     className: cn(
       "relative flex w-full touch-none select-none items-center",
       className
     ),
     ...props
-  }, React.createElement(SliderPrimitive.Track, {
+  }, createElement(Track, {
     className: "relative h-2 w-full grow overflow-hidden rounded-full bg-secondary"
-  }, React.createElement(SliderPrimitive.Range, {
+  }, createElement(Range, {
     className: "absolute h-full bg-primary"
-  })), React.createElement(SliderPrimitive.Thumb, {
+  })), createElement(Thumb, {
     className: "block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
   }))
 })
-Slider.displayName = SliderPrimitive.Root.displayName
+Slider.displayName = Root.displayName
 
-module.exports = { Slider }
+export  { Slider }
