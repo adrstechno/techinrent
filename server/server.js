ee4c8c9d45a426f57182da8eaaf5da2a7b7f48b8
Sachin Sen <sachinsen1920@gmail.com>
2025-08-06 16:13:30 +0530

    update verify

--- server/server.js
+++ server/server.js
@@ -1,44 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const cors = require('cors')
+const cors = require('cors');
 
+dotenv.config();
+const dbConnection = connectDB();
+dbConnection.catch((err) => console.error('Database connection error:', err));
 
-
-
-require('dotenv').config();
-connectDB();
-// Importing routes
-const contactRoutes = require('./routes/contactRoutes');
-const bookDemoRoutes = require('./routes/bookDemoRoute');
-const getInTouchRoutes = require('./routes/getInTouchRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const providerRoutes = require('./routes/providerRoutes')
-const formRoutes = require('./routes/formRoutes');
-const authRoutes = require('./routes/authRoutes');
-
-// Initializing express app
 const app = express();
 
 app.use(express.json());
 app.use(cors({
   origin: 'http://localhost:5173',
-  credentials: true               
+  credentials: true,
 }));
+
+const contactRoutes = require('./routes/contactRoutes');
+const bookDemoRoutes = require('./routes/bookDemoRoute');
+const getInTouchRoutes = require('./routes/getInTouchRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const providerRoutes = require('./routes/providerRoutes');
+const formRoutes = require('./routes/formRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 app.use('/api/contact', contactRoutes);
 app.use('/api/book-demo', bookDemoRoutes);
 app.use('/api/get-in-touch', getInTouchRoutes);
-app.use('/api/admin', adminRoutes);
+app.use('/api/admin', adminRoutes); // Ensure this is included
 app.use('/api/provider', providerRoutes);
 app.use('/api/form', formRoutes);
 app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
-    res.send('Backend is Running');
-})
+  res.send('Backend is Running');
+});
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT} 💙`);
-})
+  console.log(`Server is running on ${PORT} 💙`);
+});
