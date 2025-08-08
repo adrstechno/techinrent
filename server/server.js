81ddb2892dc5ba599bd9182c68017f10d73b7230
Ishantp01 <163533317+Ishantp01@users.noreply.github.com>
2025-08-08 22:31:58 +0530

    sheeets

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'https://tech-in-rent.vercel.app/'],
   credentials: true,
 }));
 
