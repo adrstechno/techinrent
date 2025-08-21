f4031acbb99e8b42d9148ac2dc947c97a9c40996
Sachin Sen <sachinsen1920@gmail.com>
2025-08-21 20:07:38 +0530

    fix toaster

--- tech/src/components/provider/BookDemoForm.jsx
+++ tech/src/components/provider/BookDemoForm.jsx
@@ -14,6 +14,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { useToast } from "@/hooks/use-toast";
+import { toast } from "react-toastify";
 
 const API_URI = "https://tech-in-rent.onrender.com";
 
