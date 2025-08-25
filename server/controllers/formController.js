8d0462fb81ffccfeb9d0a28e28ad4a723fb3b08b
Ishantp01 <163533317+Ishantp01@users.noreply.github.com>
2025-08-25 14:01:12 +0530

    form_correction

--- server/controllers/formController.js
+++ server/controllers/formController.js
@@ -7,7 +7,7 @@ exports.createForm = async (req, res) => {
     const newForm = new form({ formId });
 
     await newForm.save();
-    res.status(201).json({success: true, message: 'Form created successfully', formLink: `http://localhost:3000/form/${formId}`, formId });
+    res.status(201).json({success: true, message: 'Form created successfully', formLink: `https://tech-in-rent.vercel.app/form/${formId}`, formId });
   }
   catch(error) {
     res.status(500).json({success: false, message: 'Error creating form', error: error.message});
